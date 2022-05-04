6c9b175d095bc6958b9369593292cc30bcf14b93
Lucas Oi <lucas.oi-@hotmail.com>
2022-05-03 23:32:27 -0300

    Criado os metodos GET, POST, PUT e DELETE

--- avaliacao/src/main/java/br/com/pamcary/avaliacao/controller/UsuarioController.java
+++ avaliacao/src/main/java/br/com/pamcary/avaliacao/controller/UsuarioController.java
@@ -2,10 +2,18 @@ package br.com.pamcary.avaliacao.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +32,43 @@ public class UsuarioController {
 	public ResponseEntity <List<Usuario>> getAll(){
 		return ResponseEntity.ok(usuarioRepository.findAll());
 		// select * from tb_postagens;
-			
+	}
+	
+	
+	@GetMapping("/{id}")
+	public ResponseEntity <Usuario> getById(@PathVariable Long id){
+		return usuarioRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))//200
+				.orElse(ResponseEntity.notFound().build()); //400
+	}
+	
+	
+	@GetMapping("/nome/{nome}")
+	public ResponseEntity <List<Usuario>> getByNome(@PathVariable String nome){
+		return ResponseEntity.ok(usuarioRepository.findByNomeContainingIgnoreCase(nome));
+	}
+	
+	
+	@PostMapping
+	public ResponseEntity <Usuario> postUsuario(@Valid @RequestBody Usuario usuario){//@Valid erro 400, sem ele erro 500
+		return ResponseEntity.status(HttpStatus.CREATED).body(usuarioRepository.save(usuario));
+	}
+	
+	@PutMapping
+	public ResponseEntity <Usuario> putUsuario(@Valid @RequestBody Usuario usuario){
+		return usuarioRepository.findById(usuario.getId())
+				.map(resposta -> ResponseEntity.ok(usuarioRepository.save(usuario)))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@DeleteMapping("/{id}") // Método void
+	public ResponseEntity <?> deleteUsuario(@PathVariable Long id){
+		return usuarioRepository.findById(id)
+				.map(resposta -> {
+					usuarioRepository.deleteById(id);
+					return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+				})
+				.orElse(ResponseEntity.notFound().build());
 	}
 
 }
--- avaliacao/src/main/java/br/com/pamcary/avaliacao/repository/UsuarioRepository.java
+++ avaliacao/src/main/java/br/com/pamcary/avaliacao/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package br.com.pamcary.avaliacao.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import br.com.pamcary.avaliacao.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-
+	
+	List<Usuario> findByNomeContainingIgnoreCase(String nome);
+	/* select * from tb_usuario where usuario like "%usuario"; */
 }
